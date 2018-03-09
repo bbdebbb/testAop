1f0a50d12aa90d83bad93b549f9f39c445ff9b19
xub <843122641@qq.com>
2018-03-09 22:32:26 +0800

    000

--- src/main/java/Demo4/WordThis.java
+++ src/main/java/Demo4/WordThis.java
@@ -2,8 +2,6 @@ package Demo4;
 
 public class WordThis {
     public static void main(String[] args) {
-        System.out.println("xx");
-        System.currentTimeMillis();
-        //123123212
+
     }
 }
